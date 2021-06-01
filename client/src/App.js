4a43738718c044e3bf4a3b5723493857ff885358
Bailey Jean <baileyjleavitt@gmail.com>
2021-06-01 13:29:19 -0700

    Add componentDidMount; this.state and switch statement for updating State in client/src/App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,43 @@
-import React, { Component } from 'react'
-import './styles/App.css'
-// Your imports here
-import Nav from './components/Header'
-import { Route, Switch } from 'react-router'
-import LandingPage from './pages/LandingPage'
-import ChooseFeels from './pages/ChooseFeels'
+import axios from 'axios';
+import { BASE_URL } from './globals';
+import './styles/App.css';
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router';
+import Nav from './components/Nav';
+import LandingPage from './pages/LandingPage';
+import ChooseFeels from './pages/ChooseFeels';
+
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      actionItems: [],
+      gratitudeLists: [],
+      readingLists: [],
+      actionLists: []
+    }
+  }
+
+  async componentDidMount() {
+    const res = await axios.get(`${BASE_URL}/actions`)
+    this.setState({ actionItems: res.data.actions })
+  }
+
   render() {
+    for (let i = 0; i < this.state.actionItems.length; i++) {
+      switch (this.state.actionItems[i].type) {
+        case 'gratitudeList':
+          this.state.gratitudeLists.push(this.state.actionItems[i])
+          break
+        case 'readingList':
+          this.state.readingLists.push(this.state.actionItems[i])
+          break
+        case 'actionList':
+          this.state.actionLists.push(this.state.actionItems[i])
+          break
+        default:
+      }
+    }
     return (
       <div>
         <Nav />
