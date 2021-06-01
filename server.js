f5e946795d23723f7f46c6c0fadebcd6afd633ad
Bailey Jean <baileyjleavitt@gmail.com>
2021-05-31 17:52:29 -0700

    Fix server.js so it has deployment set-up done

--- server.js
+++ server.js
@@ -1,19 +1,33 @@
-const express = require('express');
-const routes = require('./routes');
-const db = require('./db');
-const bodyParser = require('body-parser');
-const logger = require('morgan');
-// require() imports and middleware here ^ ///////
+const express = require('express')
+const logger = require('morgan')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const routes = require('./routes')
+const db = require('./db')
+const path = require('path')
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 
-const app = express();
-app.use(bodyParser.json());
-app.use(logger('dev'));
-// app.use() middleware here ^ ///////////////////
+const app = express()
 
-app.use('/api', routes);
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(logger('dev'))
+
+// Your Code Here
+app.use('/api', routes)
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
-app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(`${__dirname}/client/build/index.html`))
+  })
+}
+
+// Your Code Ends Here
+app.listen(PORT, () => {
+  console.log(`App listening on port: ${PORT}`)
+})
