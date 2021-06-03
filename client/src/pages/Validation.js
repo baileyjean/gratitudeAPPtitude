b24de2395d13bf0e5272e010d4974a56ff30d580
Bailey Jean <baileyjleavitt@gmail.com>
2021-06-03 16:04:12 -0700

    Actionables conditionally rendering to the Validation page

--- client/src/pages/Validation.js
+++ client/src/pages/Validation.js
@@ -5,27 +5,25 @@ import { Link } from 'react-router-dom';
 export default class Validation extends Component {
   
   sortActionables = () => {
-    
+    let actionArray = [];
+    this.props.actionables.map((action) => {
+      if(action.emotionRef === this.props.match.params.id){
+        actionArray.push(action)
+      }
+    })
+    return actionArray
   }
   
   render() {
-    console.log(this.props.match.params.id)
-    const { emotions, actionables } = this.props;
+    // const { emotions, actionables } = this.props;
     return (
       <div className="container">
-        <div>
-          Your feelings are valid.
-          {actionables.map(action =>
-          <Link to={`/action/${action._id}`} key={action._id} color={`white`}>
+        {this.sortActionables().map((action, index) => (
+          <Link to={`/action/${action._id}`} key={action._id}>
             <p>{action.name}</p>
             <p>{action.description}</p>
           </Link>
-        )}
-        </div>
-        <br />
-        <a href="/choose-feels">
-          <button>Go Back</button>
-        </a>
+        )) }
       </div>
       )
   }
