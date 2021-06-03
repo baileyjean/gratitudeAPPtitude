40f75fd01d2bbeb38625f5675757dbb55c281b96
Bailey Jean <baileyjleavitt@gmail.com>
2021-06-03 15:50:47 -0700

    Got actionables rendering to page. Sorting is next

--- client/src/pages/Validation.js
+++ client/src/pages/Validation.js
@@ -3,21 +3,24 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 export default class Validation extends Component {
+  
+  sortActionables = () => {
+    
+  }
+  
   render() {
+    console.log(this.props.match.params.id)
     const { emotions, actionables } = this.props;
     return (
       <div className="container">
-        <div className="valid">
+        <div>
           Your feelings are valid.
-          {emotions.map(emotion => {
-            actionables.map(action =>
-              <Link to={`/takeaction/${emotion._id}`} key={action._id} className="emotionCard">
-                <h1>{action.name}</h1>
-                <p>{action.description}</p>
-              </Link>
-            )
-          }
-          )}
+          {actionables.map(action =>
+          <Link to={`/action/${action._id}`} key={action._id} color={`white`}>
+            <p>{action.name}</p>
+            <p>{action.description}</p>
+          </Link>
+        )}
         </div>
         <br />
         <a href="/choose-feels">
