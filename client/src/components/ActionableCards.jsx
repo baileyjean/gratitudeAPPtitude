11e48fc2b36cc3f8ec43809761db3efa13b0fdd0
Bailey Jean <baileyjleavitt@gmail.com>
2021-06-02 18:52:32 -0700

    Add ActionableCards component and begin conditional rendering for content

---
+++ client/src/components/ActionableCards.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+//import { Link } from 'react-router-dom';
+
+export default class ActionableCards extends Component {
+  render() {
+    // const { emotions } = this.props
+    // const myFeels = this.props.myFeels
+    return (
+      <div>
+        Yo what up with it
+        {/* <Link to="/validation/feeling-happy" className="emotionCard">
+          <h1>{emotions[0].name}</h1>
+          <p>{emotions[0].description}</p>
+        </Link>
+        <Link to="/validation/feeling-sad" className="emotionCard">
+          <h1>{emotions[1].name}</h1>
+          <p>{emotions[1].description}</p>
+        </Link>
+        <Link to="/validation/feeling-anger" className="emotionCard">
+          <h1>{emotions[2].name}</h1>
+          <p>{emotions[2].description}</p>
+        </Link>
+        <Link to="/validation/feeling-disgust" className="emotionCard">
+          <h1>{emotions[3].name}</h1>
+          <p>{emotions[3].description}</p>
+        </Link> */}
+      </div>
+    )
+  }
+}
--- client/src/pages/Validation.js
+++ client/src/pages/Validation.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import ActionableCards from '../components/ActionableCards';
 
 export default class Validation extends Component {
   render() {
+    const emotions = this.props.emotions
+    const myFeels = this.props.myFeels
     return (
       <div className="container">
         <div className="valid">
           Your feelings are valid.
+          <ActionableCards emotions = {emotions} myFeels = {myFeels} />
         </div>
         <br />
         <a href="/choose-feels">
