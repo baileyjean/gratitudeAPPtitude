736a17a157460e0054eababb739e2c9dd9463f08
Bailey Jean <baileyjleavitt@gmail.com>
2021-06-04 15:06:51 -0700

    Add comment to seed file to fix deployment

--- seed/comments.js
+++ seed/comments.js
@@ -4,7 +4,9 @@ const { Comment } = require('../models')
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 const main = async () => {
-  const comments = []
+  const comments = [
+    {"comment": "Wow! Gratitude APPtitude has helped me find gratitude in every situation!"}
+  ]
 
   await Comment.insertMany(comments)
   console.log('Created comments!')
